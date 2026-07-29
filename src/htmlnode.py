f391d111e07ec9eb2f7875c02a933a9f2c9ab4de
royalvarez <309090990+royalvarez@users.noreply.github.com.>
2026-07-29 01:55:40 -0500

    feat: add HTML leaf node rendering
    Introduce the LeafNode subclass requiring tag and value to be supplied,
    because tag determines how the content is wrapped in HTML and value
    determines whether rendering to HTML causes an exception or an HTML leaf
    node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,19 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str | None, value: str, props: dict[str, str] | None = None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
