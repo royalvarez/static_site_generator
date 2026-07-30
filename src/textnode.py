ef79863025ed47e8cb0d1b05ed3b818b5cb029f2
royalvarez <309090990+royalvarez@users.noreply.github.com.>
2026-07-30 02:14:27 -0500

    feat: add TextNode conversion to a LeafNode
    Add text_node_to_html_node to convert TextNode instances into LeafNode
    instances based on text type.

--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,8 @@
 
 from enum import Enum
 
+from htmlnode import LeafNode
+
 class TextType(Enum):
     TEXT = "plain"
     BOLD = "bold"
@@ -26,4 +28,25 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-    
+
+
+def text_node_to_html_node(textnode: TextNode) -> LeafNode:
+    match textnode.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, textnode.text)
+        case TextType.BOLD:
+            return LeafNode('b', textnode.text)
+        case TextType.ITALIC:
+            return LeafNode('i', textnode.text)
+        case TextType.CODE:
+            return LeafNode("code", textnode.text)
+        case TextType.LINK:
+            if textnode.url is None:
+                raise ValueError("invalid url")
+            return LeafNode('a', textnode.text, {"href": f"{textnode.url}"})
+        case TextType.IMAGE:
+            if textnode.url is None:
+                raise ValueError("invalid url")
+            return LeafNode("img", None, {"src": f"{textnode.url}", "alt": f"{textnode.text}"})
+        case _:
+            raise ValueError(f"invalid text type: {textnode.text_type}")
