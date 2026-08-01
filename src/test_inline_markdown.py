d645c5b65e47b1fef7b8dcb809d7fd50c2cec091
royalvarez <309090990+royalvarez@users.noreply.github.com.>
2026-08-01 05:28:47 -0500

    test: cover markdown images and links matching

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,7 @@
 
 import unittest
 
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 from textnode import TextNode, TextType
 
@@ -90,6 +90,100 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             [node],
             "```",
             TextType.CODE
+        )
+
+
+class TestExtractMarkdownImages(unittest.TestCase):
+    def test_extract_single_image(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertEqual(
+        [
+            ("image", "https://i.imgur.com/zjjcJKZ.png")
+        ],
+        matches
+    )
+
+    def test_extract_double_image(self):
+        matches = extract_markdown_images(
+            "These images are the same ![image](https://i.imgur.com/zjjcJKZ.png) and ![image2](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertEqual(
+            [
+                ("image", "https://i.imgur.com/zjjcJKZ.png"),
+                ("image2", "https://i.imgur.com/zjjcJKZ.png")
+            ],
+            matches
+        )
+
+    def test_extract_random_brackets(self):
+        matches = extract_markdown_images(
+            "This is [not alt text] but this is ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertEqual(
+            [
+                ("image", "https://i.imgur.com/zjjcJKZ.png")
+            ],
+            matches
+        )
+
+    def test_extract_only_image(self):
+        matches = extract_markdown_images(
+            "There is a [link](https://www.wikipedia.org) and ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertEqual(
+            [
+                ("image", "https://i.imgur.com/zjjcJKZ.png")
+            ],
+            matches
+        )
+
+
+class TestExtractMarkdownLinks(unittest.TestCase):
+    def test_extract_single_link(self):
+        matches = extract_markdown_links(
+            "This is a single link [click to go to wikipedia](https://www.wikipedia.org)"
+        )
+        self.assertEqual(
+            [
+                ("click to go to wikipedia", "https://www.wikipedia.org")
+            ],
+            matches
+        )
+
+    def test_extract_double_links(self):
+        matches = extract_markdown_links(
+            "These are two links [click to go to wikipedia](https://www.wikipedia.org) and [click to go to encyclopedia](https://www.britannica.com)"
+        )
+        self.assertEqual(
+            [
+                ("click to go to wikipedia", "https://www.wikipedia.org"),
+                ("click to go to encyclopedia", "https://www.britannica.com")
+            ],
+            matches
+        )
+
+    def test_extract_random_brackets(self):
+        matches = extract_markdown_links(
+            "This is not a link [click to go to wikipedia] but this is [click to go to wikipedia](https://www.wikipedia.org)"
+        )
+        self.assertEqual(
+            [
+                ("click to go to wikipedia", "https://www.wikipedia.org")
+            ],
+            matches
+        )
+
+    def test_extract_only_link(self):
+        matches = extract_markdown_links(
+            "There is a [link](https://www.wikipedia.org) and ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertEqual(
+            [
+                ("link", "https://www.wikipedia.org")
+            ],
+            matches
         )
 
 
