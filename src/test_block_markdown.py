64560ebfaec2f4f2e8823433252cb479211f3948
royalvarez <309090990+royalvarez@users.noreply.github.com.>
2026-08-05 21:03:46 -0500

    test: cover markdown document conversion to HTMLNodes

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,7 +1,7 @@
 
 import unittest
 
-from block_markdown import markdown_to_blocks, BlockType, block_to_block_type
+from block_markdown import markdown_to_blocks, BlockType, block_to_block_type, markdown_to_html_node
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -147,6 +147,77 @@ class TestBlockToBlockType(unittest.TestCase):
         self.assertEqual(
             BlockType.PARAGRAPH,
             block_type
+        )
+
+
+
+class TestMarkdownToHTMLNode(unittest.TestCase):
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+        tree = markdown_to_html_node(md)
+        self.assertEqual(
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+            tree
+        )
+
+    def test_headings(self):
+        md = """
+# This is a **bold** story in _italic_
+
+## This is [Bootdev](https://boot.dev)
+
+###### Remember `lang=python`
+"""
+        tree = markdown_to_html_node(md)
+        self.assertEqual(
+            "<div><h1>This is a <b>bold</b> story in <i>italic</i></h1><h2>This is <a href=\"https://boot.dev\">Bootdev</a></h2><h6>Remember <code>lang=python</code></h6></div>",
+            tree
+        )
+
+    def test_code(self):    
+        markdown = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+        tree = markdown_to_html_node(markdown)
+        self.assertEqual(
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+            tree
+        )
+
+    def test_lists(self):
+        md = """
+- Compsci
+- Phil
+
+1. Beyond
+2. Delispice
+"""
+        tree = markdown_to_html_node(md)
+        self.assertEqual(
+            "<div><ul><li>Compsci</li><li>Phil</li></ul><ol><li>Beyond</li><li>Delispice</li></ol></div>",
+            tree
+        )
+
+    def test_quote(self):
+        md = """
+> Cogito ergo sum
+> of the new order
+> ![image](https://image.png)
+"""
+        tree = markdown_to_html_node(md)
+        self.assertEqual(
+            "<div><blockquote>Cogito ergo sum of the new order <img src=\"https://image.png\" alt=\"image\"></img></blockquote></div>",
+            tree
         )
 
 
