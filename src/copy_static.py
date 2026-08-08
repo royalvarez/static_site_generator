3f781ef6503e7fdc6954fa629dd5909c5f96e77e
royalvarez <309090990+royalvarez@users.noreply.github.com.>
2026-08-08 12:43:29 -0500

    refactor: rename functions for generalization
    - copy_static_to_public is now copy_static
    - copy_static_to_public_r is now copy_static_r

--- src/copy_static.py
+++ src/copy_static.py
@@ -4,18 +4,18 @@ from pathlib import Path
 import shutil
 
 
-def copy_static_to_public(from_path: Path, destination_path: Path):
+def copy_static(from_path: Path, destination_path: Path):
     if not destination_path.exists():
         Path.mkdir(destination_path)
-    copy_static_to_public_r(from_path, destination_path)
+    copy_static_r(from_path, destination_path)
 
 
-def copy_static_to_public_r(from_path: Path, destination_path: Path):
-    for child in from_path.iterdir():
-        if child.is_dir():
-            sub_directory = child.name
-            new_destination_path = Path.joinpath(destination_path, f"{sub_directory}/")
+def copy_static_r(from_path: Path, destination_path: Path):
+    for child_path in from_path.iterdir():
+        if child_path.is_dir():
+            sub_directory_name = child_path.name
+            new_destination_path = Path.joinpath(destination_path, f"{sub_directory_name}/")
             Path.mkdir(new_destination_path)
-            copy_static_to_public_r(child, new_destination_path)
+            copy_static_r(child_path, new_destination_path)
         else:
-            shutil.copy(child, destination_path)
+            shutil.copy(child_path, destination_path)
