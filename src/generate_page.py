b7c3f2cc647aa07e72783e574761af7b349330fa
royalvarez <309090990+royalvarez@users.noreply.github.com.>
2026-08-07 22:02:16 -0500

    feat: generate pages recursively from markdown to HTML

--- src/generate_page.py
+++ src/generate_page.py
@@ -28,6 +28,52 @@ def generate_page(from_path: Path, template_path: Path, destination_path: Path):
     destination_path.write_text(template_content)
 
 
+def generate_pages_r(from_path: Path, template_path: Path, destination_path: Path):
+    if not template_path.exists():
+        raise ValueError(f"Invalid path: {template_path} could not be found")
+
+    for child_path in from_path.iterdir():
+        if child_path.is_dir():
+            new_from_path = child_path
+            new_destination_path = destination_path.joinpath(child_path.name)
+            generate_pages_r(new_from_path, template_path, new_destination_path)
+
+        else:
+            for file_path in from_path.iterdir():
+                if file_path.is_dir():
+                    continue
+                from_file_path = Path.joinpath(from_path, file_path.name)
+
+                if not str(from_file_path).endswith(".md"):
+                    raise ValueError(f"Invalid extension type: {from_path} must be markdown")
+
+                destination_file_path = Path.joinpath(destination_path, file_path.name)
+                convert_extension = str(destination_file_path).replace(".md", ".html")
+                destination_file_path = Path(convert_extension)
+            
+                print(f"Generating page from {from_file_path} to {destination_file_path} using {template_path}")
+
+                if not from_file_path.exists():
+                    raise ValueError(f"Invalid path: {from_file_path} could not be found")
+
+                markdown = from_file_path.read_text()
+
+                template_content = template_path.read_text()
+
+                html = markdown_to_html_node(markdown).to_html()
+                html_title = extract_title(markdown)
+            
+                template_content = template_content.replace("{{ Title }}", html_title)
+                template_content  = template_content.replace("{{ Content }}", html)
+
+                if not destination_path.exists():
+                    destination_path.mkdir(parents=True)
+
+                if not destination_file_path.exists():
+                    Path.touch(destination_file_path, exist_ok=True)
+                destination_file_path.write_text(template_content)
+
+
 def extract_title(markdown: str) -> str:
     lines = markdown.split("\n")
 
